02bebff41788fb184740d5a53ee7d666d2c295e5
ljnsn <info@ljnsn.com>
2024-05-29 19:21:26 +0200

    fix: sample

--- src/clients/python/samples/basic/main.py
+++ src/clients/python/samples/basic/main.py
@@ -2,6 +2,7 @@
 
 import logging
 import os
+import sys
 
 from tb_client.bindings import CreateAccountResult, CreateTransferResult
 from tb_client.client import Client
@@ -10,15 +11,15 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def main():
-    port = os.getenv("TB_PORT", "3003")
-
-    client = Client(0, [port], 256)
+def run(client: Client) -> None:
+    """Run the example."""
+    acc_id_1, acc_id_2 = 3, 4
+    tx_id = 1002
 
     results = client.create_accounts(
         [
-            {"id": 102, "ledger": 700, "code": 10},
-            {"id": 103, "ledger": 700, "code": 10},
+            {"id": acc_id_1, "ledger": 700, "code": 10},
+            {"id": acc_id_2, "ledger": 700, "code": 10},
         ]
     )
 
@@ -33,12 +34,14 @@ def main():
     if halt:
         raise RuntimeError("Error creating accounts")
 
+    logger.info("Accounts created")
+
     transfer_results = client.create_transfers(
         [
             {
-                "id": 100,
-                "debit_account_id": 102,
-                "credit_account_id": 103,
+                "id": tx_id,
+                "debit_account_id": acc_id_1,
+                "credit_account_id": acc_id_2,
                 "amount": 100,
                 "ledger": 700,
                 "code": 10,
@@ -56,6 +59,37 @@ def main():
     if halt:
         raise RuntimeError("Error creating transfers")
 
+    logger.info("Transfers created")
+
+    accounts = client.lookup_accounts([acc_id_1, acc_id_2])
+
+    logger.info("Accounts looked up: %s", accounts)
+
+    assert len(accounts) == 2
+
+    for account in accounts:
+        if account.id == acc_id_1:
+            assert account.debits_posted == 100
+            assert account.credits_posted == 0
+        elif account.id == acc_id_2:
+            assert account.debits_posted == 0
+            assert account.credits_posted == 100
+        else:
+            raise RuntimeError("Unexpected account")
+
+
+def main() -> None:
+    port = os.getenv("TB_PORT", "3003")
+    client = Client(0, [port], 256)
+
+    run(client)
+
+    # FIXME: .close() hangs trying to call deinit
+    # try:
+    #     run(client)
+    # finally:
+    #     client.close()
+
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
