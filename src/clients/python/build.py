07ad7da49200ab13cec49e92af0fb09a13c01959
ljnsn <info@ljnsn.com>
2024-05-29 20:21:42 +0200

    fix: build script

--- src/clients/python/build.py
+++ src/clients/python/build.py
@@ -7,7 +7,7 @@ this_dir = Path().absolute()
 lib_dir = this_dir.parent.joinpath("c", "lib", "x86_64-linux-gnu")
 h_file = this_dir.joinpath("tb_client", "native", "tb_client.h")
 
-ffi = cffi.FFI()
+ffibuilder = cffi.FFI()
 
 with h_file.open() as c_header:
     c_header = c_header.read()
@@ -18,10 +18,7 @@ defs = f"""\
 extern "Python" void on_completion_fn(uintptr_t, tb_client_t, tb_packet_t*, const uint8_t*, uint32_t);
 """
 
-ffi.cdef(
-    # f'{c_header}\n\nextern "Python" void on_completion_fn(uintptr_t, tb_client_t, tb_packet_t*, const uint8_t*, uint32_t);\n'
-    defs
-)
+ffibuilder.cdef(defs)
 
 head = """\
 #include <stddef.h>
@@ -30,15 +27,13 @@ head = """\
 #include "native/tb_client.h"
 """
 
-ffi.set_source(
+ffibuilder.set_source(
     "tb_client._tb_client",
     head,
     libraries=["tb_client"],
     library_dirs=[lib_dir.as_posix()],
-    # library_dirs=["tb_client/native/x86_64-linux-gnu.2.27/"],
-    # library_dirs=[this_dir.as_posix()],
     extra_link_args=[f"-Wl,-rpath,{lib_dir.as_posix()}"],
 )
 
 if __name__ == "__main__":
-    ffi.compile(verbose=True)
+    ffibuilder.compile(verbose=True)
